dd1286d8008c0969b162a42e1535299bcc0555ce
DZimo <53861899+DZimo@users.noreply.github.com>
2023-10-08 15:05:28 +0200

    add examples of source code and patches

---
+++ src/main/java/org/passau/CodeExamples/SourceCodeNullPointer.java
@@ -0,0 +1,24 @@
+package org.passau.CodeExamples;
+
+import org.passau.SourceCode.Packet;
+
+public class SourceCodeNullPointer {
+    public void method ( int a )
+    {
+        Packet packet = new Packet(a);
+        int valueInt = 1;
+        float valueFloat = 1.0f;
+        if ( a == 0 )
+        {
+            packet = null; // Faulty Localization that will cause Null pointer exception if (a != 0) later
+            valueInt = -1;
+            valueFloat = -1.0f;
+        }
+
+        else {
+            valueInt = 2;
+            valueFloat = 2.0f;
+        }
+        packet.value = a; // Null pointer exception if (a != 0)
+    }
+}
