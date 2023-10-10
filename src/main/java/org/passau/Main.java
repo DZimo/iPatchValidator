3d282adb5aaea8071a18297649ec4cfda224dc2b
DZimo <53861899+DZimo@users.noreply.github.com>
2023-10-10 18:35:07 +0200

    implemented SOOTUP in place of SOOT, CFG works properly

--- src/main/java/org/passau/Main.java
+++ src/main/java/org/passau/Main.java
@@ -1,13 +1,72 @@
 package org.passau;
 
+import org.passau.CodeExamples.SourceCodeNullPointer;
 import org.passau.SourceCode.Packet;
 import org.passau.StaticAnalyzer.ControlFlowGraph;
+import sootup.core.Project;
+import sootup.core.graph.StmtGraph;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.jimple.common.stmt.Stmt;
+import sootup.core.model.SootClass;
+import sootup.core.model.SootMethod;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.types.ClassType;
+import sootup.core.views.View;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaProject;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.JavaSootClassSource;
+import sootup.java.core.language.JavaLanguage;
+import sootup.java.core.views.JavaView;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 
 public class Main {
+
+    // PATH TO JAVA SOURCE CODE
+    private static String inputLocationPath = "C:\\Users\\Admin\\Desktop\\iPatchValidator\\target\\classes";
+
+
     public static void main(String[] args) {
+
+        //SourceCodeNullPointer srcCodeNullPointerInstance = new SourceCodeNullPointer();
+        // OLD SOOT
+        /*
         ControlFlowGraph controlFlowGraph = new ControlFlowGraph();
-        controlFlowGraph.buildCFG("org.passau.SourceCode.MainCode","method");
-        System.out.println(controlFlowGraph.getCfg());
+        controlFlowGraph.buildCFG("SourceCodeNullPointer","method", inputLocationPath);
+        //System.out.println(controlFlowGraph.getCfg());
+        */
+
+        // NEW SOOT
+
+        File file = new File(inputLocationPath);
+        if (!file.exists()) {
+            throw new IllegalStateException("Fix your path Shifat");
+        }
+        AnalysisInputLocation<JavaSootClass> inputLocation = new JavaClassPathAnalysisInputLocation(inputLocationPath); // Input for binary code
+        JavaLanguage language = new JavaLanguage(17);
+        Project project =  JavaProject.builder(language).addInputLocation(inputLocation).build();
+
+        ClassType classType =
+                project.getIdentifierFactory().getClassType("org.passau.CodeExamples.SourceCodeNullPointer"); // Set the class we want to work on
+
+        MethodSignature methodSignature = project.getIdentifierFactory().getMethodSignature( "method", "org.passau.CodeExamples.SourceCodeNullPointer", "void", Collections.singletonList("int")); // Set the method we want to work on
+
+        View view = project.createOnDemandView();// Create a view for the created project
+
+
+        SootClass<JavaSootClassSource> sootClass = (SootClass<JavaSootClassSource>) view.getClass(classType).get(); // Get the class itself
+
+        Optional<? extends SootMethod> opt = sootClass.getMethod(methodSignature.getSubSignature());
+        SootMethod method = opt.get();
+        List<Stmt> allStatements = method.getBody().getStmts();
+        StmtGraph statementGraph = method.getBody().getStmtGraph();
+
+
 
 
 
--- src/main/java/org/passau/StaticAnalyzer/ControlFlowGraph.java
+++ src/main/java/org/passau/StaticAnalyzer/ControlFlowGraph.java
@@ -1,34 +1,40 @@
 package org.passau.StaticAnalyzer;
-
+/*
 import soot.Body;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 import soot.toolkits.graph.UnitGraph;
+ */
+
+
 
 public class ControlFlowGraph {
 
     String scriptCode;
-    private ExceptionalUnitGraph cfg;
 
+    //private ExceptionalUnitGraph cfg;
 
-    public UnitGraph getCfg() {
-        return cfg;
+    public ControlFlowGraph() {
+
     }
 
-    public void buildCFG(String className, String methodName) {
-        String cp = "target/classes"; // classpath
-        Scene.v().setSootClassPath(cp);
+    /*public UnitGraph getCfg() {
+        return cfg;
+    }*/
+
+    public void buildCFG(String className, String methodName, String inputPath) {
+/*
+        Scene.v().setSootClassPath(inputPath);
         SootClass sootClass = Scene.v().loadClassAndSupport(className);
         sootClass.setApplicationClass();
         Scene.v().setMainClass(sootClass);
-
-
         SootMethod method = sootClass.getMethodByName(methodName);
         Body body = method.retrieveActiveBody();
 
         cfg = new ExceptionalUnitGraph(body);
 
+ */
     }
 }
