1ae1832d8e7653cf5aa1b7703ff27c7324841fb4
DZimo <53861899+DZimo@users.noreply.github.com>
2023-10-13 09:59:23 +0200

    add of new class SootPathsetter to set the path, and updated structure

--- src/main/java/org/passau/Main.java
+++ src/main/java/org/passau/Main.java
@@ -7,6 +7,7 @@ import org.passau.Parser.ClassModel;
 import org.passau.Parser.ClassParser;
 import org.passau.Parser.FilePathFinder;
 import org.passau.Parser.MethodModel;
+import org.passau.SootPathFinder.SootPathSetter;
 import org.passau.StaticAnalyzer.ControlFlowGraph;
 import sootup.core.Project;
 import sootup.core.graph.*;
@@ -46,12 +47,23 @@ public class Main {
     private static SootClass<JavaSootClassSource> sootClass; // The class that we want to build CFG for
 
 
-    // Constructor
+    /**
+     * Main Constructor
+     * @param className the class we want to build CFG for
+     * @param methodName the method we want to build CFG for
+     * @param typeName the type of method we want to build CFG for
+     * @param typeParameters the list of type of parameters we want to build CFG for
+     */
+
     public Main(String className, String methodName, String typeName, List<String> typeParameters) {
         constructMethodInformation(className, methodName, typeName, typeParameters);
-        initializeVariables(); // Constructor for main to initialize variables
+        buildGraph(); // Constructor for main to initialize variables
     }
 
+
+    /**
+     * Constructor for main to initialize variables that we will use to build CFG of Sootup
+     */
     public void constructMethodInformation(String classToBuildName, String methodToBuildName, String typeToBuildName, List<String> typeParameters){
         this.classToBuildName = classToBuildName;
         this.methodToBuildName= methodToBuildName;
@@ -59,7 +71,12 @@ public class Main {
         this.paramToBuildType = typeParameters;
     }
 
-    private void initializeVariables() {
+
+    /**
+     * Build Graph for SOOTUP
+     */
+
+    private void buildGraph() {
 
         AnalysisInputLocation<JavaSootClass> inputLocation = new JavaClassPathAnalysisInputLocation(INPUT_LOCATION_PATH);  // Input for binary code
         JavaLanguage language = new JavaLanguage(17);
@@ -78,26 +95,10 @@ public class Main {
         controlFlowGraph = new ControlFlowGraph(controlFlowGraph, statementGraph, allStatements, sootClass);  // Class that build CFG
     }
 
-    private static void setThePath() {
-        // Retrieve the input location path from the environment variable
-        INPUT_LOCATION_PATH = System.getenv(INPUT_LOCATION_ENV);
-        //inputLocationPath = "C:/Users/Admin/Desktop/iPatchValidator/target";
-        validatePath(INPUT_LOCATION_PATH);
-    }
-
-    private static void validatePath(String path) {
-        File file = new File(path);
-        if (path == null) {
-            throw new IllegalStateException("Environment variable " + INPUT_LOCATION_ENV + " is not set.");
-        }
-        if (!file.exists()) {
-            throw new IllegalStateException("iPatchValidator is set to a wrong path Shifat :D");
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         MutableStmtGraph graph = new MutableBlockStmtGraph();
-        setThePath(); // We first set the path and then validate it
+        SootPathSetter sootPathSetter = new SootPathSetter();  // We first set the path and then validate it
+        INPUT_LOCATION_PATH = sootPathSetter.INPUT_LOCATION_PATH;
         ClassParser classParser = new ClassParser();
         FilePathFinder filePathFinder = new FilePathFinder();
         List<String> classPaths = filePathFinder.findJavaFilePaths("C:\\Users\\Admin\\Desktop\\iPatchValidator\\src\\main\\java\\org\\passau\\CodeExamples");
@@ -116,23 +117,5 @@ public class Main {
                 controlFlowGraph.printTheControlFlowGraph(); // Print the CFG
             }
         }
-        // Build CFG for source code
-
-
-        /*
-        // Build CFG for patch code
-        Main mainInstancePatchCode = new Main("classes.org.passau.CodeExamples.PatchIFcondition", "method", "void", paramToBuildType);  // This will initialize the variables
-        graph.setStartingStmt(statementGraph.getStartingStmt());
-        System.out.println("CFG for PATCH CODE : " + sootClass.getName());
-        controlFlowGraph.printTheControlFlowGraph(); // Print the CFG
-
-
-        // Build CFG for test case code
-        Main mainInstanceTestCase = new Main("test-classes.CodeExamples.SourceCodeSimpleTest", "testMethodWithAEqualsOne", "void", paramToBuildType);  // This will initialize the variables
-        graph.setStartingStmt(statementGraph.getStartingStmt());
-        System.out.println("CFG for TEST CASE : " + sootClass.getName());
-        controlFlowGraph.printTheControlFlowGraph(); // Print the CFG
-
-         */
   }
 }
---
+++ src/main/java/org/passau/SootPathFinder/SootPathSetter.java
@@ -0,0 +1,32 @@
+package org.passau.SootPathFinder;
+
+import java.io.File;
+
+public class SootPathSetter {
+
+    public static String INPUT_LOCATION_PATH;
+    public static String INPUT_LOCATION_ENV = "iPatchValidator";
+
+    public SootPathSetter() {
+        setThePath();
+    }
+
+    private static void setThePath() {
+        // Retrieve the input location path from the environment variable
+        //inputLocationPath = "C:/Users/Admin/Desktop/iPatchValidator/target";
+
+        INPUT_LOCATION_PATH = System.getenv(INPUT_LOCATION_ENV);
+        validatePath(INPUT_LOCATION_PATH);
+    }
+
+    private static void validatePath(String path) {
+        File file = new File(path);
+        if (path == null) {
+            throw new IllegalStateException("Environment variable " + INPUT_LOCATION_ENV + " is not set.");
+        }
+        if (!file.exists()) {
+            throw new IllegalStateException("iPatchValidator is set to a wrong path Shifat :D");
+        }
+    }
+
+}
