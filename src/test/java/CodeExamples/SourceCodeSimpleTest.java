601e8ae814fb736e48c3700a625d9cf6eadac8bd
DZimo <53861899+DZimo@users.noreply.github.com>
2023-10-17 22:37:37 +0200

    fixed pom for jacoco

--- src/test/java/CodeExamples/SourceCodeSimpleTest.java
+++ src/test/java/CodeExamples/SourceCodeSimpleTest.java
@@ -4,9 +4,6 @@ import org.passau.CodeExamples.OriginalCode.SourceCodeSimple;
 
 public class SourceCodeSimpleTest {
 
-
-
-
     @Test
     public void testMethodWithAEqualsOne() {
         SourceCodeSimple sourceCodeSimple = new SourceCodeSimple();  // Replace 'YourClass' with the name of the actual class that contains the 'method'
