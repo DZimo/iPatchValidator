60110ba7b0eb62e05d5b5f82809399e0a1737fb4
Shifat Sahariar Bhuiyan <shifat.sahariar@gmail.com>
2023-10-18 09:55:42 +0200

    Test cases modified

--- src/main/java/org/passau/CodeExamples/OriginalCode/classA.java
+++ src/main/java/org/passau/CodeExamples/OriginalCode/classA.java
@@ -3,13 +3,14 @@ package org.passau.CodeExamples.OriginalCode;
 public class classA {
     int a;
 
-    public void methodA(int a){
+    public int methodA(int a){
         if( a == 0)
         {
-            a=10;
+            a= 10;
         }
         else {
             a = 99;
         }
+        return a;
     }
 }
--- src/test/java/CodeExamples/classATest.java
+++ src/test/java/CodeExamples/classATest.java
@@ -3,18 +3,20 @@ package CodeExamples;
 import org.junit.jupiter.api.Test;
 import org.passau.CodeExamples.OriginalCode.classA;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class classATest {
     @Test
     public void testMethodWithZero() {
-        classA classAinstance = new classA();  // Replace 'YourClass' with the name of the actual class that contains the 'method'
-        classAinstance.methodA(0);
-        //assertEquals("end of method\n", outContent.toString());
+        classA classAinstance = new classA();
+        int result = classAinstance.methodA(0);
+        assertEquals(10, result);
     }
 
     @Test
     public void testMethodWithOne() {
-        classA classAinstance = new classA();  // Replace 'YourClass' with the name of the actual class that contains the 'method'
-        classAinstance.methodA(1);
-        //assertEquals("end of method\n", outContent.toString());
+        classA classAinstance = new classA();
+        int result = classAinstance.methodA(1);
+        assertEquals(99, result);
     }
 }
