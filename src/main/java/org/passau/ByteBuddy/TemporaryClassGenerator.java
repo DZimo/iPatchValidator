9df433cc0afc82579b47c4ef8989a49580970a7c
Shifat Sahariar Bhuiyan <shifat.sahariar@gmail.com>
2023-10-19 01:51:53 +0200

    ByteBuddy has been updated - for each report it will create separate bytecode and class - all temporary classes save in a folder

--- src/main/java/org/passau/ByteBuddy/TemporaryClassGenerator.java
+++ src/main/java/org/passau/ByteBuddy/TemporaryClassGenerator.java
@@ -12,6 +12,7 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,6 +23,19 @@ import java.util.List;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class TemporaryClassGenerator {
+    static String sourceDirectory = System.getenv("iPatchValidator");
+    public static void main(String[] args) throws Exception {
+
+        // ITERATE OVER THE FOLDER AND GET THE CLASS BYTECODE FOR EACH REPORT
+        List<byte[]> tempClassBytesList = generateByteArrayListFromReports(sourceDirectory+"/Coverage_Reports", classA.class);
+        storeByteArrayListAsClasses(tempClassBytesList, sourceDirectory + "/TemporaryClasses");
+
+        System.out.println(Arrays.toString(tempClassBytesList.iterator().next()));
+
+
+    }
+
+
 
     public static byte[] generateTempClassFromXML(File xmlFile, Class<?> originalClass) throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -73,14 +87,61 @@ public class TemporaryClassGenerator {
     }
 
 
-    public static void main(String[] args) throws Exception {
 
-        byte[] tempClassBytes = generateTempClassFromXML(new File("coverage_report.xml"), classA.class);
 
-        System.out.println(Arrays.toString(tempClassBytes));
+    /**
+     * Generates byte arrays for all report.xml files in the specified directory using the provided class.
+     *
+     * @param directoryPath The directory where the report.xml files are located.
+     * @param targetClass The class to be used in the generation process.
+     * @return A list of byte arrays.
+     * @throws IOException If there's an issue accessing the files.
+     */
+    public static List<byte[]> generateByteArrayListFromReports(String directoryPath, Class<?> targetClass) throws Exception {
+        List<byte[]> byteArrayList = new ArrayList<>();
 
-//        Files.write( Path , tempClassBytes, new File("test1.class"));
-        FileUtils.writeByteArrayToFile(new File("test.class"), tempClassBytes);
+        File directory = new File(directoryPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            throw new IOException("The specified path is not a valid directory: " + directoryPath);
+        }
+
+        // Filter files with name matching
+        File[] reportFiles = directory.listFiles((dir, name) -> name.matches("coverage_report_.*\\.xml"));
+
+        assert reportFiles != null;
+        for (File reportFile : reportFiles) {
+            byte[] tempClassBytes = generateTempClassFromXML(reportFile, targetClass);
+            byteArrayList.add(tempClassBytes);
+        }
+
+        return byteArrayList;
+    }
+
+    /**
+     * Saves each byte array from the list as a class file within the specified directory.
+     *
+     * @param tempClassBytesList List of byte arrays representing class data.
+     * @param directoryPath Directory where the class files will be stored.
+     * @throws IOException If there's an issue writing the byte arrays to the class files.
+     */
+    public static void storeByteArrayListAsClasses(List<byte[]> tempClassBytesList, String directoryPath) throws IOException {
+        // Ensure the specified directory exists
+        File directory = new File(directoryPath);
+        if (!directory.exists()) {
+            boolean dirsCreated = directory.mkdirs();
+            if (!dirsCreated) {
+                throw new IOException("Failed to create directories: " + directory.getAbsolutePath());
+            }
+        }
+
+        // Save each byte[] as a class file within the directory
+        int classCounter = 1;
+        for (byte[] classBytes : tempClassBytesList) {
+            String classFileName = "tempClass_" + classCounter + ".class";
+            File classFile = new File(directory, classFileName);
+            FileUtils.writeByteArrayToFile(classFile, classBytes);
+            classCounter++;
+        }
     }
 
 
